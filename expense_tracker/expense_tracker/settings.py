87fd13c378c800234ca9f2f6f7822b2fda0e3777
Shourya <sdeepbera@gmail.com>
2025-03-08 17:20:41 +0530

    Updates for hosting

--- expense_tracker/expense_tracker/settings.py
+++ expense_tracker/expense_tracker/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a=5jad$n9d1@(6%5f(iadgx#lt+bdnn+4!%#ioj)-_p6xwry(5'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+#SECRET_KEY = 'django-insecure-a=5jad$n9d1@(6%5f(iadgx#lt+bdnn+4!%#ioj)-_p6xwry(5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower()=="true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -74,6 +75,7 @@ WSGI_APPLICATION = 'expense_tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -85,6 +87,9 @@ DATABASES = {
     }
 }
 
+databaase_url = os.environ.get("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(databaase_url)
+#postgresql://data_q5wg_user:9RLNyDZTp24VBnmmsW2czkGzuZ6IBhOB@dpg-cv61du2n91rc73ba30ag-a.oregon-postgres.render.com/data_q5wg
 
 
 # Password validation
