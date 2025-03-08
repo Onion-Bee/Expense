01f754045e3759d9770091bd0771da1745531509
Shourya <sdeepbera@gmail.com>
2025-03-08 18:04:19 +0530

    changed the hardcoded back to the environment get

--- expense_tracker/expense_tracker/settings.py
+++ expense_tracker/expense_tracker/settings.py
@@ -26,10 +26,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower()=="true"
 
-ALLOWED_HOSTS = ["https://expense-tracker-nw99.onrender.com/"]
-CSRF_TRUSTED_ORIGINS = [
-    "https://expense-tracker-nw99.onrender.com/"
-]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+
 
 # Application definition
 INSTALLED_APPS = [
